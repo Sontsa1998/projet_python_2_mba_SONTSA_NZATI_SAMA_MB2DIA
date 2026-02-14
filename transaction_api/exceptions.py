25b381adee4237bcbda750a07b32b762d6893f1d
Camelia SAMA <camelia.sama@epitech.>
2026-02-14 22:08:47 +0100

    feat:exceptions

--- transaction_api/exceptions.py
+++ transaction_api/exceptions.py
@@ -5,3 +5,8 @@ class TransactionAPIException(Exception):
     """Base exception for Transaction API."""
 
     pass
+
+class TransactionNotFound(TransactionAPIException):
+    """Exception raised when a transaction is not found."""
+
+    pass
