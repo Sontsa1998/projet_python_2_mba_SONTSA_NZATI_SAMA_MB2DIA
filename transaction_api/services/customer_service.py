30cb35bbaac8b3dbea0687a82d6215a2f2ac96da
Nzati stephane <stephnzati@gmail.com>
2026-02-14 14:03:48 +0100

    add: imports et initialisation di logger

---
+++ transaction_api/services/customer_service.py
@@ -0,0 +1,18 @@
+"""Customer service for business logic."""
+
+from typing import List
+
+from transaction_api.exceptions import InvalidPaginationParameters
+from transaction_api.logging_config import get_logger
+from transaction_api.models import (
+    Customer,
+    CustomerSummary,
+    PaginatedResponse,
+    TopCustomer,
+)
+from transaction_api.pagination import PaginationService
+from transaction_api.repository import TransactionRepository
+
+logger = get_logger(__name__)
+
+
