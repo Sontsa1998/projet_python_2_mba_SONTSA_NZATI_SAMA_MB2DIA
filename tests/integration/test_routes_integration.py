d6fa59890b3022939f00b3bb2763468703f1f3d2
Nzati stephane <stephnzati@gmail.com>
2026-02-14 15:48:10 +0100

    add: import

---
+++ tests/integration/test_routes_integration.py
@@ -0,0 +1,9 @@
+"""Integration tests for all API routes."""
+
+import pytest
+from fastapi.testclient import TestClient
+from transaction_api.main import app
+from transaction_api import app_context
+from transaction_api.repository import TransactionRepository
+
+
